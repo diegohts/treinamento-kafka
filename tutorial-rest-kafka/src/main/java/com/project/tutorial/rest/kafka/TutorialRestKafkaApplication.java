ce23375379462d1b03b519268eff662af1b6a98a
Diego Henrique <diegohrp20@gmail.com>
2023-01-25 22:56:51 -0300

    Commit inicial

---
+++ tutorial-rest-kafka/src/main/java/com/project/tutorial/rest/kafka/TutorialRestKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.project.tutorial.rest.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialRestKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialRestKafkaApplication.class, args);
+	}
+
+}
