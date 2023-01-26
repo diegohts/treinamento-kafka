912b37e6d9c59f0be4233504f559ec67c1d33959
Diego Henrique <diegohrp20@gmail.com>
2023-01-26 01:00:31 -0300

    Conectando e enviando mensagem de salva pedido ao realizar a requisicao POST dando Sucesso

--- tutorial-rest-kafka/src/main/java/com/project/tutorial/rest/kafka/controller/PedidosController.java
+++ tutorial-rest-kafka/src/main/java/com/project/tutorial/rest/kafka/controller/PedidosController.java
@@ -1,12 +1,22 @@
-package controller;
+package com.project.tutorial.rest.kafka.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.project.tutorial.rest.kafka.data.PedidoData;
+import com.project.tutorial.rest.kafka.service.RegistraEventoService;
+import lombok.RequiredArgsConstructor;
 
 @RestController
+@RequiredArgsConstructor
 public class PedidosController {
 
-    public ResponseEntity<String> salvarPedido() {
+    private final RegistraEventoService eventoService;
+
+    @PostMapping(path = "/api/salvaPedido")
+    public ResponseEntity<String> salvarPedido(@RequestBody PedidoData pedido) {
+        eventoService.adicionarEvento("SalvarPedido", pedido);
         return ResponseEntity.ok("Sucesso");
     }
 }
