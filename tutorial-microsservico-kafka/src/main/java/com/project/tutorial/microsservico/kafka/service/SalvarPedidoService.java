f6c8a0a50f3a3bdb56d9ce6a77c8f015429e6e9e
Diego Henrique <diegohrp20@gmail.com>
2023-01-26 08:24:27 -0300

    Desenvolvendo o serviço de consumidor

---
+++ tutorial-microsservico-kafka/src/main/java/com/project/tutorial/microsservico/kafka/service/SalvarPedidoService.java
@@ -0,0 +1,11 @@
+package com.project.tutorial.microsservico.kafka.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalvarPedidoService {
+
+    private void executar() {
+
+    }
+}
---
+++ tutorial-microsservico-kafka/src/test/java/com/project/tutorial/microsservico/kafka/TutorialMicrosservicoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.tutorial.microsservico.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialMicrosservicoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
